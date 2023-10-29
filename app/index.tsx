039303efce9257d142e867838e2789c7b692703b
Marcelo Soares <marcelosoaresinc@gmail.com>
2023-10-29 02:44:10 -0300

    fix: verification

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,23 @@
 import { AuthContext } from 'context/useAuthContext';
 import { Redirect } from 'expo-router'
-import { useContext, useEffect } from 'react';
+import { User, onAuthStateChanged } from 'firebase/auth';
+import { FIREBASE_AUTH } from 'firebaseConfig';
+import { useContext, useEffect, useState } from 'react';
+import { View } from 'react-native';
 
 export default function Page() {
-
-  const { user, getCurrentUser } = useContext(AuthContext)
-
-  useEffect(() => {
-    getCurrentUser
-  }, [])
+  const [user, setUser] = useState<User | null>(null)
   
-  return !user ? <Redirect href={'/(auth)/login'}/> : <Redirect href={'/favorites/favorites'}/>
+  useEffect(() => {
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      setUser(user)
+    })
+  }, [])
+
+
+  return (
+    <View>
+      {user ? <Redirect href={'/(auth)/login'}/> : <Redirect href={'/(tabs)/home/home'}/>}
+    </View>
+  )
 }
